4819e62e5a6f44b84bf81b1290bdc86a8e87ef15
Luiz Gonzaga Franco Michelmann <39537987+lgfranco22@users.noreply.github.com>
2021-08-24 02:34:47 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<html> 
+	<body> 
+		<form action="envia.php" method="post" enctype="multipart/form-data"> 
+			<input type="file" name="uploaded" id="Arquivo"><br> 
+			<input type="submit" value="Enviar"> 
+			<input type="reset" value="Apagar"> 
+		</form> 
+	</body> 
+</html>
